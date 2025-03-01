ab8ca980505bb1830aa2b75e90a0067c30a3a839
Zain Ul Abdeen <zu4425@gmail.com>
2025-03-01 22:19:49 +0500

    26:23

---
+++ blog app backend/WebApplication1/WebApplication1/Migrations/20250301171344_Init.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "catogries",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_catogries", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Conetent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    images = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isfeatured = table.Column<bool>(type: "bit", nullable: false),
+                    catogreysId = table.Column<int>(type: "int", nullable: false),
+                    catogriesid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_blogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_blogs_catogries_catogriesid",
+                        column: x => x.catogriesid,
+                        principalTable: "catogries",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_blogs_catogriesid",
+                table: "blogs",
+                column: "catogriesid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blogs");
+
+            migrationBuilder.DropTable(
+                name: "catogries");
+        }
+    }
+}
---
+++ blog app backend/WebApplication1/WebApplication1/Model/catogries.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Model
+{
+    [Table("catogries")]
+    public class catogries
+    {
+        public int id { get; set; }
+
+        public String Name { get; set; }
+    }
+}
