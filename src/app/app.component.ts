baa4811b34309d0981537148c6e1ada53c7a506f
Zain Ul Abdeen <zu4425@gmail.com>
2025-03-04 20:57:30 +0500

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from "./Components/header/header.component";
+import { FooterComponent } from "./Components/footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, HeaderComponent, FooterComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { AboutComponent } from './Components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:"blog",component:BlogComponent},
+    {path:"blogs",component:BlogComponent},
+    {path:"about" ,component:AboutComponent}
+];
