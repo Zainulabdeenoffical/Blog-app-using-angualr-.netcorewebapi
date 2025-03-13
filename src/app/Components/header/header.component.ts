f5682cf033c5cf397e335841a6c742c39df013bd
Zain Ul Abdeen <zu4425@gmail.com>
2025-03-13 10:22:51 +0500

    Merge branch 'master' of https://github.com/Zainulabdeenoffical/Blog-app-using-angualr-.netcorewebapi

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-header',
+  imports : [RouterModule,MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './Components/home/home.component';
+import { HomeComponent } from './Components/Home/home.component';
 import { BlogComponent } from './Components/blog/blog.component';
-import { AboutComponent } from './Components/about/about.component';
+import { AboutComponent } from './Components/About/about.component';
+import { BlogsComponent } from './Components/blogs/blogs.component';
 
 export const routes: Routes = [
-    {path:"",component:HomeComponent},
-    {path:"blog",component:BlogComponent},
-    {path:"blogs",component:BlogComponent},
-    {path:"about" ,component:AboutComponent}
+
+        {path:' ',component:HomeComponent},
+        { path:'home',component: HomeComponent },
+        { path: 'blog', component: BlogComponent },
+        { path: 'blogs', component: BlogsComponent },
+        { path: 'about', component: AboutComponent },
+        { path: '**', redirectTo: 'home', pathMatch:'full' }
+      
+      
 ];
